df14f6fe56cf0f723ca05c87fafa771030172530
syifakstudentitpens <syifak18@it.student.pens.ac.id>
2021-12-16 16:47:39 +0700

    first commit

---
+++ database/seeds/PaketSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class PaketSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('paket')->insert([
+            'id' => 1,
+            'ket' => 'Bilingual',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+        DB::table('paket')->insert([
+            'id' => 2,
+            'ket' => 'Reguler',
+            'created_at' => date('Y-m-d H:i:s'),
+            'updated_at' => date('Y-m-d H:i:s')
+        ]);
+    }
+}
---
+++ resources/views/home.blade.php
@@ -0,0 +1,113 @@
+@extends('template_backend.home')
+@section('heading', 'Dashboard')
+@section('page')
+  <li class="breadcrumb-item active">Dashboard</li>
+@endsection
+@section('content')
+    
+
+   
+
+    
+@endsection
+@section('script')
+    <script>
+      $(document).ready(function () {
+        setInterval(function() {
+          var date = new Date();
+          var hari = date.getDay();
+          var h = date.getHours();
+          var m = date.getMinutes();
+          h = (h < 10) ? "0" + h : h;
+          m = (m < 10) ? "0" + m : m;
+          var jam = h + ":" + m;
+          
+          if (hari == '0' || hari == '6') {
+            $("#data-jadwal").html(
+              `<tr>
+                <td colspan='5' style='background:#fff;text-align:center;font-weight:bold;font-size:18px;'>Sekalah Libur!</td>
+              </tr>`
+            );
+          } else {
+            if (jam <= '07:00') {
+              $("#data-jadwal").html(
+                `<tr>
+                  <td colspan='5' style='background:#fff;text-align:center;font-weight:bold;font-size:18px;'>Jam Pelajaran Hari ini Akan Segera Dimulai!</td>
+                </tr>`
+              );
+            } else if (
+              hari == '1' && jam >= '16:15' ||
+              hari == '2' && jam >= '16:00' ||
+              hari == '3' && jam >= '16:00' ||
+              hari == '4' && jam >= '16:00' ||
+              hari == '5' && jam >= '15:40'
+            ) {
+              $("#data-jadwal").html(
+                `<tr>
+                  <td colspan='5' style='background:#fff;text-align:center;font-weight:bold;font-size:18px;'>Jam Pelajaran Hari ini Sudah Selesai!</td>
+                </tr>`
+              );
+            } else {
+              if (
+                hari == '1' && jam >= '09:45' && jam <= '10:15' ||
+                hari == '1' && jam >= '12:30' && jam <= '13:15' ||
+                hari == '2' && jam >= '09:15' && jam <= '09:45' ||
+                hari == '2' && jam >= '12:00' && jam <= '13:00' ||
+                hari == '3' && jam >= '09:15' && jam <= '09:45' ||
+                hari == '3' && jam >= '12:00' && jam <= '13:00' ||
+                hari == '4' && jam >= '09:15' && jam <= '09:45' ||
+                hari == '4' && jam >= '12:00' && jam <= '13:00' ||
+                hari == '5' && jam >= '09:00' && jam <= '09:15' ||
+                hari == '5' && jam >= '11:15' && jam <= '13:00'
+              ) {
+                $("#data-jadwal").html(
+                  `<tr>
+                    <td colspan='5' style='background:#fff;text-align:center;font-weight:bold;font-size:18px;'>Waktunya Istirahat!</td>
+                  </tr>`
+                );
+              } else if (hari == '1' && jam >= '07:00' && jam <= '07:30') {
+                $("#data-jadwal").html(
+                  `<tr>
+                    <td colspan='5' style='background:#fff;text-align:center;font-weight:bold;font-size:18px;'>Waktunya Upacara Bendera!</td>
+                  </tr>`
+                );
+              } else {
+                $.ajax({
+                  type:"GET",
+                  data: {
+                    hari : hari,
+                    jam : jam
+                  },
+                  dataType:"JSON",
+                  url:"{{ url('/jadwal/sekarang') }}",
+                  success:function(data){
+                    var html = "";
+                    $.each(data, function (index, val) {
+                        html += "<tr>";
+                          html += "<td>" + val.jam_mulai + ' - ' + val.jam_selesai + "</td>";
+                          html += "<td><h5 class='card-title'>" + val.mapel + "</h5><p class='card-text'><small class='text-muted'>" + val.guru + "</small></p></td>";
+                          html += "<td>" + val.kelas + "</td>";
+                          html += "<td>" + val.ruang + "</td>";
+                          if (val.ket != null) {
+                            html += "<td><div style='margin-left:20px;width:30px;height:30px;background:#"+val.ket+"'></div></td>";
+                          } else {
+                            html += "<td></td>";
+                          }
+                        html += "</tr>";
+                    });
+                    $("#data-jadwal").html(html);
+                  },
+                  error:function(){
+                  }
+                });
+              }
+            }
+          }
+        }, 60 * 1000);
+      });
+      
+      $("#Dashboard").addClass("active");
+      $("#liDashboard").addClass("menu-open");
+      $("#Home").addClass("active");
+    </script>
+@endsection
